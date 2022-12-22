65cd9cd918dda33fe77f59ac44e6044e714b32c9
dooleydan <johnston.daniel97@gmail.com>
2022-12-21 23:51:29 -0600

    Final JS and HTML Update
    Added prompt variables and loops.

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
 var password = document.getElementByID("password");
+var confirmUppercase;
+var confirmLowercase;
+var confirmNumber;
+var confirmSpecialCharacter;
 
+
+// Prompt user to choose num between 8 and 128
+        
+// This function will generate the password
 function genPassword(){
+    // variables to create the 4 prompts asked for: Upper, Lower, Number, Special Character
+   var confirmUppercase = confirm("Click OK to include uppercase letters.");
+   var confirmLowercase = confirm("Click OK to include lowercase letters.");
+   var confirmNumber = confirm("Click OK to include numbers")
+   var confirmSpecialCharacter = confirm("Click OK to include special characters.");
+    //loop if answer is outside the parameters
+    while(confirmUppercase === false && confirmLowercase === false && confirmNumber === false && confirmSpecialCharacter === false){
+        alert("You must choose at least one");
+        var confirmUppercase = confirm("Click OK to include uppercase letters.");
+        var confirmLowercase = confirm("Click OK to include lowercase letters.");
+        var confirmNumber = confirm("Click OK to include numbers")
+        var confirmSpecialCharacter = confirm("Click OK to include special characters.");
+    }
+    // var to create password character possibilities
     var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    // var to save length of password
     var passwordLength = 12;
+    // var to reset password regen
     var password ="";
-
+    // Create a for loop that will wipe out the old password and generate a new one
     for (var i = 0; i<= passwordLength; i++){
+        //randomNumber variable needed to formulate new password
         var randomNumber = Math.floor(Math.random() * chars.length);
         password += chars.substring(randomNumber, randomNumber +1);
     }
@@ -13,14 +38,6 @@ function genPassword(){
     document.getElementById("password").value = password;
 }
 
-//This wil activate the copy button
-
-function copyPassword(){
-    var copyText = document.getElementById("password");
-    copyText.select();
-    copyText.setSelectionRange(0,999);
-    document.copyText("copy");
-}
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
@@ -40,9 +57,7 @@ function generatePassword(){
     // Git add commit and push repository often
     // Lots of fors and if thens
     // must return a string value that is at the password.
-    // Prompt user to choose num between 8 and 128
-        // var to save length
-        // validate user number
+    
     // Confirm usser upper, lower ,number, special characters (4 confirm prompts)
         // 4 vars to save true/false to included characers
         // Validate that atleast one category was chosen
